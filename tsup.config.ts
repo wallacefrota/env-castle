191fe95e55c07c5b1691352b392cb23fdc7028c1
Wallace Frota <frotadev@gmail.com>
2026-03-20 15:14:48 -0300

    Refactor tsup configuration to use an object for entry points, enhancing clarity and maintainability.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: {
+    index: 'src/index.ts',
+  },
   format: ['esm', 'cjs'],
   dts: true,
   clean: true,
